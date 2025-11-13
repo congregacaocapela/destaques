86d9912a8b7ee5f348acafbbbfd2cd7347044c61
congregacaocapela <congregacaocapela@gmail.com>
2025-11-13 13:47:16 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// Este script serve como o "garçom" que entrega
-// os arquivos estáticos do seu [site] bucket.
-export default {
-  async fetch(request, env, ctx) {
-    // Permite que o próprio Cloudflare lide com a entrega dos arquivos
-    return env.ASSETS.fetch(request);
-  },
-};
